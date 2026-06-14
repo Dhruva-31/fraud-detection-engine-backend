f33c013005a37363136bb3dbe28dfe8caf34a95d
Dhruva U R <dhruvakrishna63@gmail.com>
2026-06-14 13:23:27 +0530

    fix: console log in controller

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -8,10 +8,13 @@ const saveTransaction = async (req, res) => {
   const userId = req.user.userId;
   const io = req.app.get("io");
 
-  const { transaction, fraudResult } = await saveTransactionService({
-    ...req.body,
-    userId
-  }, io);
+  const { transaction, fraudResult } = await saveTransactionService(
+    {
+      ...req.body,
+      userId,
+    },
+    io,
+  );
 
   res.status(201).json({
     message: "Transaction saved successfully",
@@ -37,7 +40,6 @@ const getTransactionById = async (req, res) => {
   const userId = req.user.userId;
 
   const transaction = await getTransactionByIdService(id, userId);
-  console.log(transaction);
 
   res.status(200).json({
     message: "Transaction retrievel successfully",
